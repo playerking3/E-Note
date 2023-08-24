a8cf32f2b483ac6b640dd73e85c0ea4ef367d6ca
playerking3 <murilomarquesdecastro@gmail.com>
2023-08-24 10:56:11 -0300

    terminei o header

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,5 +1,26 @@
 pesquisa = document.getElementById('pesquisa')
+popup = document.getElementById('criar')
 
-async function show(){
+function show(){
 	pesquisa.style.visibility = 'visible'
+}
+
+function habilitar(element){
+	if (element.checked){
+		document.getElementById('prazo').disabled = false
+	}
+	else{
+		document.getElementById('prazo').disabled = true
+	}
+}
+
+function abrir(){
+	popup.style.top = '0%'
+	document.getElementById("escuro").style.display = "inline-block"
+}
+
+function fechar(btn){
+    let pai = btn.parentElement
+    popup.style.top = '-100%'
+    document.getElementById("escuro").style.display = "none"
 }
